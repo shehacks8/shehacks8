ea07f893a248b0ff866fa9e93b54c163ee848b32
Rhea Mangat <rmanga4@uwo.ca>
2023-12-08 11:55:56 -0500

    mlh badge

--- app/(homepage)/hacker-olympics/page.tsx
+++ app/(homepage)/hacker-olympics/page.tsx
@@ -46,7 +46,7 @@ export default function HackerOlympics() {
       >
       <Link href = "https://mlh.io/seasons/2023/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=black">
         <Image 
-          src = {"https://s3.amazonaws.com/logged-assets/trust-badge/2023/mlh-trust-badge-2023-white.svg"}
+          src = {"https://s3.amazonaws.com/logged-assets/trust-badge/2024/mlh-trust-badge-2024-white.svg"}
           height = {[ '45px', '55px', '75px', '100px', '100px', '150px']} 
           width = {[ '45px', '55px', '75px', '75px', '75px', '125px']} 
           // sizes = {"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"}
